fd13364eb84af2e361533d5d4e9df4483e72ddc0
Chirag Davé <chirage@gmail.com>
2014-07-17 00:47:06 -0700

    Some dashboard stuff and table WIP

--- quinoa/DashboardViewController.h
+++ quinoa/DashboardViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface DashboardViewController : UIViewController
+@interface DashboardViewController : UIViewController<UITableViewDataSource, UITableViewDelegate>
 
 @end
--- quinoa/DashboardViewController.m
+++ quinoa/DashboardViewController.m
@@ -7,11 +7,17 @@
 //
 
 #import "DashboardViewController.h"
+#import "UserHeader.h"
+#import "User.h"
 
 
 @interface DashboardViewController ()
 
 @property (weak, nonatomic) IBOutlet UITableView *feedTable;
+@property (strong, nonatomic) NSArray *likes;
+@property (strong, nonatomic) User *user;
+@property (strong, nonatomic) User *expert;
+@property (strong, nonatomic) UserHeader *expertHeader;
 
 @end
 
@@ -22,6 +28,9 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+        _user = [User currentUser];
+        _expert = _user.currentTrainer;
+        [_expert fetch];
     }
     return self;
 }
@@ -30,12 +39,30 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.feedTable.dataSource = self;
+    self.feedTable.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark UITableViewDataSource methods
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.likes count];
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TableViewCell" forIndexPath:indexPath];
+    return cell;
+}
+
+#pragma mark UITableViewDelegate methods
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    self.expertHeader = [[UserHeader alloc] init];
+    [self.expertHeader setUser:self.expert];
+    return self.expertHeader;
 }
 
 @end

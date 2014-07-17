84194f282f32916547aa43ae497d49259175077a
Chirag Davé <chirage@gmail.com>
2014-07-17 02:09:05 -0700

    loading likes wip

---
+++ quinoa/ActivityLikeCell.h
@@ -0,0 +1,13 @@
+//
+//  ActivityLikeCell.h
+//  quinoa
+//
+//  Created by Chirag Davé on 7/17/14.
+//  Copyright (c) 2014 3eesho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivityLikeCell : UITableViewCell
+
+@end
---
+++ quinoa/ActivityLikeCell.m
@@ -0,0 +1,25 @@
+//
+//  ActivityLikeCell.m
+//  quinoa
+//
+//  Created by Chirag Davé on 7/17/14.
+//  Copyright (c) 2014 3eesho. All rights reserved.
+//
+
+#import "ActivityLikeCell.h"
+
+@implementation ActivityLikeCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end

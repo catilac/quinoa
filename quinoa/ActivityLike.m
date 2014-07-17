28f8f8b18348b647c70afd1d97af5702e82f59e1
Chirag Davé <chirage@gmail.com>
2014-07-17 01:42:03 -0700

    Make expert a pointer; add class methods for creating and fetchingActivityLikes

--- quinoa/ActivityLike.m
+++ quinoa/ActivityLike.m
@@ -12,11 +12,38 @@
 @implementation ActivityLike
 
 @dynamic user;
-@dynamic expertId;
+@dynamic expert;
 @dynamic activity;
 
 + (NSString *)parseClassName {
     return @"ActivityLike";
+}
+
++ (void)getActivityLikesByUser:(User *)user
+                       success:(void (^)(NSArray *activityLikes))success
+                         error:(void (^)(NSError *error))error {
+    PFQuery *query = [ActivityLike query];
+    [query whereKey:@"user" equalTo:user];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *activityLikes, NSError *errorFromParse) {
+        if (success) {
+            success(activityLikes);
+        }
+        if (errorFromParse) {
+            error(errorFromParse);
+        }
+    }];
+}
+
++ (ActivityLike *)likeActivity:(Activity *)activity
+                          user:(User *)user
+                        expert:(User *)expert {
+    ActivityLike *newActivityLike = [ActivityLike object];
+    newActivityLike.user = user;
+    newActivityLike.expert = expert;
+    newActivityLike.activity = activity;
+    [newActivityLike saveInBackground];
+
+    return newActivityLike;
 }
 
 @end

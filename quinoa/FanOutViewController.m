d8471dde1cc0e4a545f3dbd40975292990070151
Amie Kweon <amie.kweon@rd.io>
2014-07-17 22:48:36 -0700

    Update activity input screen with blurred background

--- quinoa/FanOutViewController.m
+++ quinoa/FanOutViewController.m
@@ -16,6 +16,7 @@
 @interface FanOutViewController ()
 
 @property (weak, nonatomic) IBOutlet UIButton *trackFoodButton;
+@property (strong, nonatomic) UIImage *currentBackgroundImage;
 
 - (IBAction)onTrackFood:(id)sender;
 - (IBAction)onTrackWeight:(id)sender;
@@ -29,8 +30,6 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
-        //[self.view setBackgroundColor:[UIColor colorWithRed:0.4 green:0.8 blue:0.2 alpha:0.6]];
     }
     return self;
 }
@@ -40,17 +39,22 @@
     [super viewDidLoad];
 }
 - (void)viewWillAppear:(BOOL)animated {
+
+    [super viewWillAppear:animated];
+    self.navigationController.interactivePopGestureRecognizer.enabled = NO;
     [self blurBackground];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
+
+    [super viewWillDisappear:animated];
+    self.navigationController.interactivePopGestureRecognizer.enabled = YES;
     [self undoBlurBackground];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (IBAction)onTrackFood:(id)sender {
@@ -60,67 +64,33 @@
 
 - (IBAction)onTrackWeight:(id)sender {
     
-    ActivityViewController *activityVC = [[ActivityViewController alloc] initWithType:@"trackWeight"];
-    CGSize imageSize = CGSizeZero;
-    
-    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
-    if (UIInterfaceOrientationIsPortrait(orientation)) {
-        imageSize = [UIScreen mainScreen].bounds.size;
-    } else {
-        imageSize = CGSizeMake([UIScreen mainScreen].bounds.size.height, [UIScreen mainScreen].bounds.size.width);
-    }
-    
-    UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0);
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    for (UIWindow *window in [[UIApplication sharedApplication] windows]) {
-        CGContextSaveGState(context);
-        CGContextTranslateCTM(context, window.center.x, window.center.y);
-        CGContextConcatCTM(context, window.transform);
-        CGContextTranslateCTM(context, -window.bounds.size.width * window.layer.anchorPoint.x, -window.bounds.size.height * window.layer.anchorPoint.y);
-        if (orientation == UIInterfaceOrientationLandscapeLeft) {
-            CGContextRotateCTM(context, M_PI_2);
-            CGContextTranslateCTM(context, 0, -imageSize.width);
-        } else if (orientation == UIInterfaceOrientationLandscapeRight) {
-            CGContextRotateCTM(context, -M_PI_2);
-            CGContextTranslateCTM(context, -imageSize.height, 0);
-        } else if (orientation == UIInterfaceOrientationPortraitUpsideDown) {
-            CGContextRotateCTM(context, M_PI);
-            CGContextTranslateCTM(context, -imageSize.width, -imageSize.height);
-        }
-        if ([window respondsToSelector:@selector(drawViewHierarchyInRect:afterScreenUpdates:)]) {
-            [window drawViewHierarchyInRect:window.bounds afterScreenUpdates:YES];
-        } else {
-            [window.layer renderInContext:context];
-        }
-        CGContextRestoreGState(context);
-    }
-    
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
+    ActivityViewController *trackViewController = [[ActivityViewController alloc] initWithType:@"trackWeight"];
 
-    activityVC.image = image;
-    //activityVC.backgroundImageView.image = image;
-    //activityVC.view.backgroundColor = [UIColor clearColor];
-    //[self.view insertSubview:activityVC.view aboveSubview:self.view];
-
-    [self.navigationController pushViewController:activityVC animated:YES];
-
-    
+    UIImageView * backgroundImageView = [[UIImageView alloc] initWithImage:self.currentBackgroundImage];
+    [trackViewController.view addSubview:backgroundImageView];
+    [trackViewController.view sendSubviewToBack:backgroundImageView];
+    [self.navigationController pushViewController:trackViewController animated:NO];
 }
 
 - (IBAction)onTrackActivity:(id)sender {
-    ActivityViewController *activityVC = [[ActivityViewController alloc] initWithType:@"trackActivity"];
-    
-    [self.navigationController pushViewController:activityVC animated:YES];
+
+    ActivityViewController *activityViewController = [[ActivityViewController alloc] initWithType:@"trackActivity"];
+
+    UIImageView * backgroundImageView = [[UIImageView alloc] initWithImage:self.currentBackgroundImage];
+    [activityViewController.view addSubview:backgroundImageView];
+    [activityViewController.view sendSubviewToBack:backgroundImageView];
+    [self.navigationController pushViewController:activityViewController animated:NO];
 }
 
 - (void)blurBackground {
-    UIImage *bgImage = [Utils screenshot];
+    if (self.currentBackgroundImage == nil) {
+        UIImage *bgImage = [Utils screenshot];
 
-    // Tweak these values Nathan!
-    UIColor *tintColor = [UIColor colorWithWhite:0.1 alpha:0.5];
-    bgImage = [bgImage applyBlurWithRadius:10 tintColor:tintColor saturationDeltaFactor:1.8 maskImage:nil];
-    self.view.backgroundColor = [UIColor colorWithPatternImage:bgImage];
+        // Tweak these values Nathan!
+        UIColor *tintColor = [UIColor colorWithWhite:0.1 alpha:0.5];
+        self.currentBackgroundImage = [bgImage applyBlurWithRadius:10 tintColor:tintColor saturationDeltaFactor:1.8 maskImage:nil];
+    }
+    self.view.backgroundColor = [UIColor colorWithPatternImage:self.currentBackgroundImage];
 }
 
 - (void)undoBlurBackground {

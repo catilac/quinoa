16fbb5cf466bf8b260923375b581cfebc6a60b0a
Chirag Davé <chirage@gmail.com>
2014-07-18 22:37:33 -0700

    Displaying basic info on Dashboard, and graph. Need to polish, and use actual user data

--- quinoa/DashboardViewController.m
+++ quinoa/DashboardViewController.m
@@ -32,7 +32,7 @@ static NSString *LikeCellIdent = @"likeCellIdent";
 @property (strong, nonatomic) UIView *statsBar;
 @property (strong, nonatomic) UILabel *weightDifferential;
 @property (strong, nonatomic) UILabel *totalActiveTime;
-@property (strong, nonatomic) UILabel *numActivityLikes;
+@property (strong, nonatomic) UILabel *numKudos;
 
 
 
@@ -64,6 +64,12 @@ static NSString *LikeCellIdent = @"likeCellIdent";
     [self fetchActivityLikes];
     [self fetchWeightStats];
     
+    self.title = @"Dashboard";
+    
+    // I can only make the navigation bar opaque by setting it on each page
+    self.navigationController.navigationBar.translucent = NO;
+    self.tabBarController.tabBar.translucent = NO;
+
 }
 
 - (void)setupDashboardHeader {
@@ -76,7 +82,6 @@ static NSString *LikeCellIdent = @"likeCellIdent";
     [chartLabel setFont:[UIFont fontWithName:@"SourceSansPro-Regular" size:12.0f]];
     chartLabel.text = @"Weight over Last 7 Days";
     
-    
     // Initialize BarChart
     CGRect barChartFrame = CGRectMake(5, 5, dashFrame.size.width-10, dashFrame.size.height*0.75f);
     self.barChart = [[PNBarChart alloc] initWithFrame:barChartFrame];
@@ -85,15 +90,69 @@ static NSString *LikeCellIdent = @"likeCellIdent";
     [self.dashboardHeader addSubview:self.barChart];
 
     // Initialize Stats Bar
-    CGRect statsFrame = CGRectMake(5, barChartFrame.size.height, dashFrame.size.width-10, dashFrame.size.height*0.25f);
+    CGRect statsFrame = CGRectMake(5, barChartFrame.size.height, dashFrame.size.width-10, 83);
     self.statsBar = [[UIView alloc] initWithFrame:statsFrame];
-//    [self.statsBar setBackgroundColor:[Utils getVibrantBlue]];
+    self.statsBar.backgroundColor = [Utils getGreen];
     
+    // Weight Info
+    CGRect weightFrame = CGRectMake(25, 0, 40, 30);
+    UIView *weightInfo = [[UIView alloc] initWithFrame:weightFrame];
+
+    self.weightDifferential = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 40, 16)];
+    self.weightDifferential.text = @"-5lbs";
+    [self.weightDifferential setFont:[UIFont fontWithName:@"SourceSansPro-Regular" size:24.0f]];
+    self.weightDifferential.textColor = [Utils getDarkBlue];
+    [self.weightDifferential sizeToFit];
+    self.weightDifferential.backgroundColor = [Utils getVibrantBlue];
     
+    UILabel *weightLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, self.weightDifferential.frame.size.height, 40, 10)];
+    weightLabel.text = @"Weight";
+    weightLabel.font = [UIFont fontWithName:@"SourceSansPro-Regular" size:18.0f];
+    [weightLabel sizeToFit];
+    
+    [weightInfo addSubview:self.weightDifferential];
+    [weightInfo addSubview:weightLabel];
+    [self.statsBar addSubview:weightInfo];
+    
+    // Total Active Time Label
+    CGRect timeFrame = CGRectMake(25+weightFrame.size.width+40, 0, 80, 30);
+    UIView *activeTimeInfo = [[UIView alloc] initWithFrame:timeFrame];
+    self.totalActiveTime = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 40, 16)];
+    self.totalActiveTime.text = @"00h 00m";
+    self.totalActiveTime.font = [UIFont fontWithName:@"SourceSansPro-Regular" size:24.0f];
+    self.totalActiveTime.textColor = [Utils getDarkBlue];
+    [self.totalActiveTime sizeToFit];
+    
+    UILabel *activeTimeLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, self.totalActiveTime.frame.size.height, 40, 10)];
+    activeTimeLabel.text = @"Active Time";
+    activeTimeLabel.font = [UIFont fontWithName:@"SourceSansPro-Regular" size:18.0f];
+    [activeTimeLabel sizeToFit];
+    
+    [activeTimeInfo addSubview:self.totalActiveTime];
+    [activeTimeInfo addSubview:activeTimeLabel];
+    [self.statsBar addSubview:activeTimeInfo];
+    
+    // Number Kudos Label
+    CGRect kudosRect = CGRectMake(25+timeFrame.size.width + 100, 0, 40, 30);
+    UIView *kudosInfo = [[UIView alloc] initWithFrame:kudosRect];
+    
+    self.numKudos = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 40, 16)];
+    self.numKudos.text = @"100";
+    self.numKudos.font = [UIFont fontWithName:@"SourceSansPro-Regular" size:24.0f];
+    self.numKudos.textColor = [Utils getDarkBlue];
+    [self.numKudos sizeToFit];
+    
+    UILabel *kudosLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, self.numKudos.frame.size.height, 40, 10)];
+    kudosLabel.text = @"Kudos";
+    kudosLabel.font = [UIFont fontWithName:@"SourceSansPro-Regular" size:18.0f];
+    [kudosLabel sizeToFit];
+    
+    [kudosInfo addSubview:self.numKudos];
+    [kudosInfo addSubview:kudosLabel];
+    [self.statsBar addSubview:kudosInfo];
+
     [self.view addSubview:self.dashboardHeader];
-    
     [self.view addSubview:self.statsBar];
-    
 }
 
 - (void)fetchWeightStats {

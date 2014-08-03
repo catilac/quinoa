465b6534b22e2c833945ca993ca0d3f536294cd1
Chirag Davé <chirage@gmail.com>
2014-08-03 14:00:03 -0700

    Quinoa Flow Layout subclass

---
+++ quinoa/QuinoaFlowLayout.h
@@ -0,0 +1,13 @@
+//
+//  QuinoaFlowLayout.h
+//  quinoa
+//
+//  Created by Chirag Davé on 8/3/14.
+//  Copyright (c) 2014 3eesho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuinoaFlowLayout : UICollectionViewFlowLayout
+
+@end
---
+++ quinoa/QuinoaFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  QuinoaFlowLayout.m
+//  quinoa
+//
+//  Created by Chirag Davé on 8/3/14.
+//  Copyright (c) 2014 3eesho. All rights reserved.
+//
+
+#import "QuinoaFlowLayout.h"
+
+@implementation QuinoaFlowLayout
+
+@end
